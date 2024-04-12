9f7c7e7ef97c84df3b27db3d67af8f47f1767216
TPODAvia <thepowerofdarknes2000@gmail.com>
2024-04-12 19:58:52 +0300

    task generator update

--- moveit_python/src/task_constructor/task_executer_json.py
+++ moveit_python/src/task_constructor/task_executer_json.py
@@ -1,130 +1,142 @@
 #!/usr/bin/env python3
 import rospy
 import actionlib
+import json
 from moveit_msgs.msg import MoveItErrorCodes
 from moveit_msgs.msg import PickupAction, PickupGoal, PlaceAction, PlaceGoal
 from geometry_msgs.msg import PoseStamped
-from moveit_python import MoveGroupInterface
-from moveit_python import PickPlaceInterface
-from moveit_python import PlanningSceneInterface
+from moveit_python import MoveGroupInterface, PickPlaceInterface, PlanningSceneInterface
 import time
 import sys
 from std_msgs.msg import Float64
-import json
 
+# Initialize ROS node
 rospy.init_node('pick_and_place_node')
 scene = PlanningSceneInterface("/base_link")
 move_group_interface = MoveGroupInterface(group="fr10_arm", frame="world")
 pick_place_interface = PickPlaceInterface(group="fr10_arm", ee_group="gripper")
-
 pub = rospy.Publisher('/rh_p12_rn_position/command', Float64, queue_size=10)
 
-def spawn_obj(x,y,z):
-    scene.addBox("hello_box", 0.05, 0.05, 0.05, x, y, z, use_service=True)
+def task(mode_list, name_list, content_list):
+    for i in len(mode_list):
+        target_task = name_list[i]
+        content_task = content_list[i]
 
-def attach_obj():
-    scene.attachBox("hello_box", 0.05, 0.05, 0.05, 0, 0, 0, "rh_p12_rn_tf_end")
-        
-def remove_obj_all():
-    scene.clear()
+        if mode_list[i] == "get_joints_position":
+            print("task_executer_json.py: get_joints_position")
+            joint_list = []
+            pos_list = []
+            for joint, pos in content_task.items():
+                joint_list.append(joint)
+                pos_list.append(pos)
+            move_group_interface.moveToJointPosition(joints=[joint_list], positions=[pos_list], tolerance=0.01, wait=True)
+            time.sleep(5)
 
-def remove_collis_all():
-    for name in scene.getKnownCollisionObjects():
-        print("Removing %s" % name)
-        scene.removeCollisionObject(name, use_service=False)
-    scene.waitForSync()
+        elif mode_list[i] == "get_end_coordinate":
+            print("task_executer_json.py: get_end_coordinate")
+            for loc_type, coordinate in content_task.items():
+                if loc_type == "position":
+                    position = coordinate
+                elif loc_type == "quaternion":
+                    quaternion = coordinate
+                else:
+                    print("get_end_coordinate error")
+                    sys.exit()
+            pick_pose = PoseStamped()
+            pick_pose.header.frame_id = "world"
+            pick_pose.pose.position.x = position[0]
+            pick_pose.pose.position.y = position[1]
+            pick_pose.pose.position.z = position[2]
+            pick_pose.pose.orientation.x = quaternion[0]
+            pick_pose.pose.orientation.y = quaternion[1]
+            pick_pose.pose.orientation.z = quaternion[2]
+            pick_pose.pose.orientation.w = quaternion[3]
+            result = move_group_interface.moveToPose(pick_pose, gripper_frame=target_task, tolerance=0.01, wait=True)
+            if result.error_code.val < 1:
+                print(f"task_executer.py Error: {result.error_code}")
+                sys.exit()
 
-def remove_collision(name):
-    scene.removeCollisionObject(name)
+        elif mode_list[i] == "spawn_object":
+            print("task_executer_json.py: spawn_object")
+            axis_list = []
+            value_list = []
+            for axis, value in content_task.items():
+                axis_list.append(axis)
+                value_list.append(value)
+            scene.addBox(target_task, 0.05, 0.05, 0.05, value_list[0], value_list[1], value_list[2], use_service=True)
+            time.sleep(5)
 
-def remove_attached(name):
-    scene.removeAttachedObject(name)
+        elif mode_list[i] == "attach_object":
+            print("task_executer_json.py: attach_object")
+            # scene.attachBox(target_task, 0.05, 0.05, 0.05, 0, 0, 0, "rh_p12_rn_tf_end")
+            scene.attachBox(target_task, 0.05, 0.05, 0.05, 0, 0, 0, content_task)
+            time.sleep(5)
 
-def gripper_open():
-    msg = Float64()
-    msg.data = 0.0
-    print(f"task_executer.py: Open: {msg.data}")
-    pub.publish(msg)
+        elif mode_list[i] == "detach_object":
+            scene.removeAttachedObject(target_task)
+            axis_list = []
+            value_list = []
+            for axis, value in content_task.items():
+                axis_list.append(axis)
+                value_list.append(value)
+            scene.addBox(target_task, 0.05, 0.05, 0.05, value_list[0], value_list[1], value_list[2], use_service=True)
+            time.sleep(5)
 
-def gripper_close(value):
-    msg = Float64()
-    if value < 0:
-        msg.data = 0.68 # set to max
-    else:
-        msg.data = value
-    print(f"task_executer.py: Close: {msg.data}")
-    pub.publish(msg)
+        elif mode_list[i] == "gripper_open":
+            msg = Float64()
+            msg.data = content_task
+            print(f"task_executer.py: Open: {msg.data}")
+            pub.publish(msg)
 
-def task():
-    #---------------------------------------------------------------------------
-    x=0
-    y=0.5
-    z=0.2
-    remove_attached("hello_box")
-    remove_obj_all()
-    time.sleep(5)
-    spawn_obj(x,y,z)
-    gripper_open()
-    #---------------------------------------------------------------------------
-    # OMPL Algorithm is set as default
-    move_group_interface.setPipelineId("ompl")
-    move_group_interface.setPlannerId("RRTConnect")
-    #---------------------------------------------------------------------------
-    print("task_executer.py: Move to zero positions")
-    move_group_interface.moveToJointPosition(joints=["j1","j2","j3","j4","j5","j6"], positions=[0,-1.57,1.57,0,0,0], tolerance=0.01, wait=True)
-    time.sleep(5)
-    #---------------------------------------------------------------------------
-    print("task_executer.py: pick_pose")
-    # remove_collision("hello_box")
-    # time.sleep(5)
-    pick_pose = PoseStamped()
-    pick_pose.header.frame_id = "world"
-    pick_pose.pose.position.x = x
-    pick_pose.pose.position.y = y
-    pick_pose.pose.position.z = z
-    pick_pose.pose.orientation.w = 1.0
-    result = move_group_interface.moveToPose(pick_pose, gripper_frame="rh_p12_rn_tf_end", tolerance=0.01, wait=True)
-    if result.error_code.val < 1:
-        print(f"task_executer.py Error: {result.error_code}")
-        sys.exit()
-    #---------------------------------------------------------------------------
-    print("task_executer.py: attach_obj")
-    attach_obj()
-    gripper_open()
-    time.sleep(5)
-    #--------------------------------------------------------------------------- 
-    move_group_interface.setPipelineId("pilz_industrial_motion_planner")
-    move_group_interface.setPlannerId("LIN")
-    #---------------------------------------------------------------------------
-    print("task_executer.py: place_pose")
-    place_pose = PoseStamped()
-    place_pose.header.frame_id = "world"
-    place_pose.pose.position.x = x
-    place_pose.pose.position.y = y
-    place_pose.pose.position.z = z+0.3
-    place_pose.pose.orientation.w = 1.0
-    result = move_group_interface.moveToPose(place_pose, gripper_frame="rh_p12_rn_tf_end", tolerance=0.01, wait=True, max_velocity_scaling_factor=0.1, max_acceleration_scaling_factor=0.1)
+        elif mode_list[i] == "gripper_close":
+            msg = Float64()
+            if value < 0:
+                msg.data = content_task
+            else:
+                msg.data = value
+            print(f"task_executer.py: Close: {msg.data}")
+            pub.publish(msg)
 
-    if result.error_code.val < 1:
-        print(f"task_executer.py Error: {result.error_code}")
-        sys.exit()
-    #---------------------------------------------------------------------------
-    remove_attached("hello_box")
-    gripper_open()
-    spawn_obj(x,y,z+0.3)
-    time.sleep(5)
-    #---------------------------------------------------------------------------
-    move_group_interface.setPipelineId("ompl")
-    move_group_interface.setPlannerId("RRTConnect")
-    #---------------------------------------------------------------------------
-    print("task_executer.py: Move to zero positions")
-    result = move_group_interface.moveToJointPosition(joints=["j1","j2","j3","j4","j5","j6"], positions=[0,-1.57,1.57,0,0,0], tolerance=0.01, wait=True)
-    time.sleep(5)
-    #---------------------------------------------------------------------------
-    remove_obj_all()
+        elif mode_list[i] == "choose_pipeline":
+            move_group_interface.setPipelineId(target_task)
+            move_group_interface.setPlannerId(content_task)
 
-    print("task_executer.py: Finish!")
-    
+        elif mode_list[i] == "choose_follow_mode":
+            pass
+
+        elif mode_list[i] == "clear_scene":
+            scene.clear()
+
+        else:
+            print("Content error")
 
 if __name__ == '__main__':
-    task()
+    file_path = "/home/vboxuser/catkin_ws/src/frcobot_ros/moveit_python/tasks/fr10/test.json"
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    mode_list = []
+    name_list = []
+    content_list = []
+    while not rospy.is_shutdown():
+        if isinstance(data, list):
+            for index, item in enumerate(data):
+                # Assuming the first name is the first key in each item
+                for timer, content in item.items():
+                    # print(f"\n")
+                    for mode, content2 in content.items():
+                        mode_list.append(mode)
+                        for content_name, content3 in content2.items():
+                            name_list.append(content_name)
+                            content_list.append(content3)
+        break
+    
+    print("*"*40)
+    print(mode_list)
+    print("*"*40)
+    print(name_list)
+    print("*"*40)
+    print(content_list)
+    print("*"*40)
+    task(mode_list, name_list, content_list)
+    sys.exit()
--- moveit_python/src/task_constructor/task_generator.py
+++ moveit_python/src/task_constructor/task_generator.py
@@ -4,30 +4,20 @@ import rospy
 import time
 import os
 from std_msgs.msg import Float64
-# from gazebo_msgs.srv import GetEntityState
-# from gazebo_msgs.msg import EntityState
-from geometry_msgs.msg import Pose
-from geometry_msgs.msg import Point
-from geometry_msgs.msg import Quaternion
 import json
 from sensor_msgs.msg import JointState
 import sys
 import json
 from tf import TransformListener
+from moveit_python import PlanningSceneInterface
+
 ######################################################################################################
 class TaskGenerator():
     def __init__(self, robot_name, mode, *argv):
         rospy.init_node('task_geberator_node', anonymous=True)
 
         print(f"task_geberator_node: | robot:{robot_name} | mode:{mode} |")
-        # Check if there are any arguments passed
-        if len(sys.argv) > 1:
-            # Iterate over all arguments starting from the second one (index 1)
-            for i in range(3, len(sys.argv)):
-                print(f"Argument #{i} is {sys.argv[i]}")
-        else:
-            print("Task_geberator_node: No additional command-line arguments provided.")
-
+        self.arguments = sys.argv
         self.robot = robot_name
         self.home_dir = os.path.expanduser('~')
         self.mode = mode
@@ -49,7 +39,8 @@ class TaskGenerator():
             "choose_pipeline": self.choose_pipeline,
             "choose_follow_mode": self.choose_follow_mode,
             "clear_scene": self.clear_scene,
-            "check_json_file": self.check_json_file
+            "check_json_file": self.check_json_file,
+            "detele_json_sim_content": self.detele_json_sim_content,
         }
 
         action = mode_actions.get(mode)
@@ -60,30 +51,36 @@ class TaskGenerator():
             print("Available modes:", list(mode_actions.keys()))
             sys.exit()
 
+    def save_json(self, joint_data_local):
+        save_path = f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json'
+        try:
+            with open(save_path, 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = []
+        except json.JSONDecodeError:
+            # Attempt to repair or bypass corrupted JSON data
+            print(f"File are corrupted: {save_path}")
+            sys.exit()
+
+        data.append(joint_data_local)
+        with open(save_path, 'w') as file:
+            json.dump(data, file, indent=1)
+
     def get_joints_position(self):
         self.subscriber = rospy.Subscriber("/joint_states", JointState, self.get_joints_position_callback)
         while not rospy.is_shutdown():
             if ((self.name is not None) and (self.position is not None)):
+                position_list = {}
                 for name, position in zip(self.name, self.position):
-                    self.joint_data1[name] = position
+                    position_list[name] = position
+                self.joint_data1["positions"] = position_list
                 self.joint_data2[self.mode] = self.joint_data1
                 self.joint_data3[time.time()] = self.joint_data2
 
-                save_path = f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json'
-
-                try:
-                    with open(save_path, 'r') as file:
-                        data = json.load(file)
-                except FileNotFoundError:
-                    data = []
-                data.append(self.joint_data3)
-
-                with open(save_path, 'w') as file:
-                    json.dump(data, file, indent=1)
-
+                self.save_json(self.joint_data3)
                 print("get_joints_position finished")
                 break
-
             self.rate.sleep()
         sys.exit()
 
@@ -92,97 +89,192 @@ class TaskGenerator():
         self.position = msg.position
 
     def get_end_coordinate(self):
-        listener = TransformListener()
-        listener.waitForTransform("/map", "/base_link", rospy.Time(), rospy.Duration(4.0))
-        position, quaternion = listener.lookupTransform("/map", f"/{self.argv}", rospy.Time())
-        self.joint_data[self.argv] = {position, quaternion}
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
-        print("get_end_coordinate finished")
+        if len(self.arguments) == 4:
+            # Iterate over all arguments starting from the second one (index 1)
+            target = str(self.arguments[len(sys.argv)-1])
+            print(f"Argument is {self.arguments[len(sys.argv)-1]}")
 
+            listener = TransformListener()
+            listener.waitForTransform("/world", f"/{target}", rospy.Time(), rospy.Duration(5.0))
+            position, quaternion = listener.lookupTransform("/world", f"/{target}", rospy.Time())
+            self.joint_data1[target] = {'position': position, 'quaternion': quaternion}
+            self.joint_data2[self.mode] = self.joint_data1
+            self.joint_data3[time.time()] = self.joint_data2
+            self.save_json(self.joint_data3)
+            print("get_end_coordinate finished")
+            sys.exit()
+        else:
+            print("Arguments error")
+            print("Example: rosrun moveit_python task_generator.py fr10 get_end_coordinate rh_p12_rn_tf_end")
+            sys.exit()
+    
     def spawn_object(self):
-        # self.joint_data["spawn_object"] = {box, location}
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
+        if len(self.arguments) == 7:
+            xyz = [0,0,0]
+            for i in range(4,len(self.arguments)):
+                print(self.arguments[i])
+                xyz[i-4]=self.arguments[i]
+            scene = PlanningSceneInterface("/base_link")
+            scene.addBox(self.arguments[3], 0.05, 0.05, 0.05, xyz[0], xyz[1], xyz[2], use_service=True)
+            self.joint_data1[self.arguments[3]] = {'x':xyz[0],'y':xyz[1],'z':xyz[2]}
+            self.joint_data2[self.mode] = self.joint_data1
+            self.joint_data3[time.time()] = self.joint_data2
+            self.save_json(self.joint_data3)
+            print("get_end_coordinate finished")
+            sys.exit()
+        else:
+            print("Arguments error")
+            print("Example: rosrun moveit_python task_generator.py fr10 spawn_object hello_box 0 0.5 0.2")
+            sys.exit()
 
     def attach_object(self):
-        # self.joint_data["attach_object"] = {object_name, attach_link}
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
+        if len(self.arguments) == 5:
+            scene = PlanningSceneInterface("/base_link")
+            scene.attachBox(self.arguments[3], 0.05, 0.05, 0.05, 0, 0, 0, "rh_p12_rn_tf_end")
+            self.joint_data1[self.arguments[3]] = self.arguments[4]
+            self.joint_data2[self.mode] = self.joint_data1
+            self.joint_data3[time.time()] = self.joint_data2
+            self.save_json(self.joint_data3)
+            print("get_end_coordinate finished")
+            sys.exit()
+        else:
+            print("Arguments error")
+            print("Example: rosrun moveit_python task_generator.py fr10 attach_object hello_box rh_p12_rn_tf_end")
+            sys.exit()
 
     def detach_object(self):
-        # self.joint_data["detach_object"] = {object_name, attach_link}
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
+        if len(self.arguments) == 5:
+            # scene = PlanningSceneInterface("/base_link")
+            # scene.removeAttachedObject(self.arguments[3])
+            # get coordinate fo the end
+            # scene.addBox(self.arguments[3], 0.05, 0.05, 0.05, x, y, z, use_service=True)
+            self.joint_data1[self.arguments[3]] = self.arguments[4]
+            self.joint_data2[self.mode] = self.joint_data1
+            self.joint_data3[time.time()] = self.joint_data2
+            self.save_json(self.joint_data3)
+            print("get_end_coordinate finished")
+            sys.exit()
+        else:
+            print("Arguments error")
+            print("Example: rosrun moveit_python task_generator.py fr10 detach_object hello_box rh_p12_rn_tf_end")
+            sys.exit()
 
     def clear_scene(self):
-        # all or specific joint
-        # clear all scene
-        pass
+        scene = PlanningSceneInterface("/base_link")
+        scene.clear()
 
     def gripper_open(self):
         pub = rospy.Publisher('/rh_p12_rn_position/command', Float64, queue_size=10)
+        sub = rospy.Subscriber('/rh_p12_rn_position/state', Float64, self.gripper_callback)
         msg = Float64()
         msg.data = 0.0
-        rospy.loginfo(f"Publishing: {msg.data}")
-        pub.publish(msg)
-        self.joint_data["gripper_open"] = msg.data
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
+        while not rospy.is_shutdown():
+            rospy.loginfo(f"Publishing: {msg.data}")
+            pub.publish(msg)
+            if self.position == msg.data:
+                break
+
+        self.joint_data1[self.arguments[2]] = 0
+        self.joint_data2[self.mode] = self.joint_data1
+        self.joint_data3[time.time()] = self.joint_data2
+        self.save_json(self.joint_data3)
+        print("gripper_open finished")
+        sys.exit()
 
     def gripper_close(self):
         pub = rospy.Publisher('/rh_p12_rn_position/command', Float64, queue_size=10)
+        sub = rospy.Subscriber('/rh_p12_rn_position/state', Float64, self.gripper_callback)
         msg = Float64()
         msg.data = 0.68
-        rospy.loginfo(f"Publishing: {msg.data}")
-        pub.publish(msg)
-        self.joint_data["gripper_close"] = msg.data
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
+        while not rospy.is_shutdown():
+            rospy.loginfo(f"Publishing: {msg.data}")
+            pub.publish(msg)
+            if self.position == msg.data:
+                break
 
+        self.joint_data1[self.arguments[2]] = 0.68
+        self.joint_data2[self.mode] = self.joint_data1
+        self.joint_data3[time.time()] = self.joint_data2
+        self.save_json(self.joint_data3)
+        print("gripper_close finished")
+        sys.exit()
+        
+    def gripper_callback(self, msg):
+        self.position = round(msg.command,2)
 
     def choose_pipeline(self):
-        self.joint_data["choose_pipeline"] = pipeline_name
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
-
-    def choose_follow_mode(self, folder_link, folder_name, result):
-        self.joint_data["choose_follow_mode"] = true or false
-        with open(f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json', "+w")  as f:
-            json.dump(self.joint_data, f)
+        if (len(self.arguments) == 5) and (not self.arguments[3] in ["OMPL", "PILZ"]):
+            self.joint_data1[self.arguments[3]] = self.arguments[4]
+            self.joint_data2[self.mode] = self.joint_data1
+            self.joint_data3[time.time()] = self.joint_data2
+            self.save_json(self.joint_data3)
+            print("choose_pipeline finished")
+            sys.exit()
+        else:
+            print("Arguments error")
+            print("Example: rosrun moveit_python task_generator.py fr10 choose_pipeline OMPL RRTConnect")
+            print("Example: rosrun moveit_python task_generator.py fr10 choose_pipeline PILZ LIN")
+            sys.exit()
+        
+    def choose_follow_mode(self):
+        self.joint_data2[self.mode] = 1
+        self.joint_data3[time.time()] = self.joint_data2
+        self.save_json(self.joint_data3)
+        print("choose_follow_mode finished")
+        sys.exit()
 
     def check_json_file(self):
-        directory = f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/test.json'
-        invalid_json_files = []
-        valid_json_files = []
-        
-        for filename in os.listdir(directory):
-            if filename.endswith('.json'): # Ensure we're only checking JSON files
-                file_path = os.path.join(directory, filename)
-                try:
-                    with open(file_path, 'r') as file:
-                        json.load(file) # Attempt to parse the file as JSON
-                    valid_json_files.append(filename)
-                except ValueError as e:
-                    print(f"Invalid JSON file: {filename} - Error: {e}")
-                    invalid_json_files.append(filename)
-        
-        print(f"Valid JSON files: {valid_json_files}")
-        print(f"Invalid JSON files: {invalid_json_files}")
+        if (len(self.arguments) == 4):
+            directory = f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/{self.arguments[3]}.json'
+            invalid_json_files = []
+            valid_json_files = []
+            
+            for filename in os.listdir(directory):
+                if filename.endswith('.json'): # Ensure we're only checking JSON files
+                    file_path = os.path.join(directory, filename)
+                    try:
+                        with open(file_path, 'r') as file:
+                            json.load(file) # Attempt to parse the file as JSON
+                        valid_json_files.append(filename)
+                    except ValueError as e:
+                        print(f"Invalid JSON file: {filename} - Error: {e}")
+                        invalid_json_files.append(filename)
+            
+            print(f"Valid JSON files: {valid_json_files}")
+            print(f"Invalid JSON files: {invalid_json_files}")
+            print("check_json_file finished")
+            sys.exit()
+        else:
+            print("Arguments error")
+            print("Example: rosrun moveit_python task_generator.py fr10 check_json_file test")
+            sys.exit()
 
-def print_error():
-    print("Error usage: rosrun moveit_python task_generator.py arg1 arg1 arg3")
-    print("Available arg1: fr3, fr10")
-    print("Available arg2: ")
-    print("Available arg3: for specific functions")
-    sys.exit()
-
+    def detele_json_sim_content(self):
+        directory = f'{self.home_dir}/catkin_ws/src/frcobot_ros/moveit_python/tasks/{self.robot}/{self.arguments[3]}.json'
+        with open(directory, 'r') as file:
+            data = json.load(file) # Attempt to parse the file as JSON
+        print(data)
+        
 if __name__ == "__main__":
     if len(sys.argv) < 3: # Assuming TaskGenerator requires three arguments plus the script name
         print("Error usage: rosrun moveit_python task_generator.py arg1 arg2 arg3 arg4 etc..")
         sys.exit()
     else:
         _, robot, mode, *arguments = sys.argv
+        if robot == "help":
+            print("Usage example:")
+            print("rosrun moveit_python task_generator.py fr10 get_joints_position")
+            print("rosrun moveit_python task_generator.py fr10 get_end_coordinate rh_p12_rn_tf_end")
+            print("rosrun moveit_python task_generator.py fr10 spawn_object hello_box 0 0.5 0.2")
+            print("rosrun moveit_python task_generator.py fr10 clear_scene")
+            print("rosrun moveit_python task_generator.py fr10 gripper_open")
+            print("rosrun moveit_python task_generator.py fr10 gripper_close")
+            print("rosrun moveit_python task_generator.py fr10 choose_pipeline OMPL RRTConnect")
+            print("rosrun moveit_python task_generator.py fr10 choose_pipeline PILZ LIN")
+            print("rosrun moveit_python task_generator.py fr10 choose_follow_mode")
+            print("rosrun moveit_python task_generator.py fr10 check_json_file test")
+            print("rosrun moveit_python task_generator.py fr10 detele_json_sim_content test")
+            sys.exit()
         if not robot in ["fr3", "fr10"]:
             print("Error usage: arg1: [fr3, fr10]")
             sys.exit()
@@ -191,13 +283,3 @@ if __name__ == "__main__":
             sys.exit()
         TaskGenerator(robot, mode, *arguments)
         rospy.spin()
-
-
-# 1 print joints position
-# 2 print link* global coordinate
-# 3 gripper on/off
-# 4 simulated grasp attach/release
-# 5 change planner solution pilz, ompl
-# 6 clear scene
-# 8 perseption follower mode
-# 9 check json file
